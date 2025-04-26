ebf86ce9856597448b26058136507512047455bf
Dat-Okin <94436958+Dat-Okin@users.noreply.github.com>
2025-04-26 00:34:55 -0400

    real round 2
    if its not clear i have no idea what im doing yet

--- js/layers.js
+++ js/layers.js
@@ -1,15 +1,15 @@
 addLayer("s", {
-    name: "Mana Shards", // This is optional, only used in a few places, If absent it just uses the layer id.
+    name: "Mana Shard", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "S", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
 		points: new Decimal(0),
     }},
-    color: "#1B4994",
+    color: "#0F2CBD",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
-    resource: "mana shards", // Name of prestige currency
-    baseResource: "mana", // Name of resource prestige is based on
+    resource: "Mana Shards", // Name of prestige currency
+    baseResource: "Mana", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
@@ -22,27 +22,8 @@ addLayer("s", {
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
-        {key: "s", description: "S: Condense some mana into shards", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+        {key: "s", description: "S: Condense Mana into Shards", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
-    upgrades: {
-        11: {
-            title: "Mana Boost",
-            description: "Shards are giving you mana.",
-            cost: new Decimal(5),
-
-        },
-        12: {
-            title: "Boost Boost",
-            description: "Makes Shards more potent",
-            cost: new Decimal(20),
-                effect() {
-        return player[this.layer].points.add(1).pow(0.5)
-    },
-    effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
-
-
-        },
-       
-    },
     layerShown(){return true}
+    
 })
--- js/mod.js
+++ js/mod.js
@@ -42,8 +42,6 @@ function getPointGen() {
 		return new Decimal(0)
 
 	let gain = new Decimal(1)
-	if (hasUpgrade('s', 12)) gain = gain.times(upgradeEffect('s', 12))
-	if (hasUpgrade('s', 11)) gain = gain.times(2)
 
 	
 		return gain
